b1f1b09260470045b054fd69ae77f284fa835445
Sylvain <sylvain.germe@gmail.com>
2014-04-12 18:26:10 +0200

    Get Last Follower

--- lib/twitbot/queue.rb
+++ lib/twitbot/queue.rb
@@ -1,20 +1,27 @@
 module Twitbot
   class Queue < SimpleDelegator
-    def initialize()
+  	attr_accessor :queue
+  
+
+    def initialize(twitbot_user)
     	# @redis_client = Redis.New(url:'redis://redistogo:dfecc27c174f80919e08b2e41ee5f5f2@grideye.redistogo.com:10595/')
+    	@queue =  'queue-' + twitbot_user.id
     	super(Twitbot.redis)
     end
 
     def enqueue(var)
-    	Twitbot.redis.rpush('mylist',var)
+    	# Put one message onto the queue
+    	Twitbot.redis.rpush(@queue,var)
     end
 
     def dequeue(block = false)
+    	# Get First message from the queue
     	if block
-    		Twitbot.redis.blpop()
+    		Twitbot.redis.blpop(@queue)
     	else
-    		Twitbot.redis.lpop()
+    		Twitbot.redis.lpop(@queue)
     	end
     end
+    
   end
 end
