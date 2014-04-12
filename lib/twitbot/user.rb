4d7cd7944c34d60e3dfd64d73d7bade087f603ec
Alexis Montagne <alexis.montagne@gmail.com>
2014-04-12 14:22:05 +0200

    Wrap User actions within a class

--- lib/twitbot/user.rb
+++ lib/twitbot/user.rb
@@ -1,6 +1,22 @@
 module Twitbot
-  class User
-    def initialize(id = 'me')
+  class User < SimpleDelegator
+    attr_accessor :user
+
+    def initialize(id = nil)
+      @user = Twitbot.rest_client.user(id)
+      super(@user)
+    end
+
+    def follow(id = nil)
+      Twitbot.rest_client.follow(id)
+    end
+
+    def followers(id = nil)
+      Twitbot.rest_client.followers(id)
+    end
+
+    def followings(id = nil)
+      Twitbot.rest_client.friends(id)
     end
   end
 end
--- spec/twitbot/user_spec.rb
+++ spec/twitbot/user_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Twitbot::User do
+  it "should forward methods to the delegated user" do
+    object = NullObject.new
+    Twitbot.rest_client.stub(:user) { object }
+    object.should_receive(:foo).once
+    Twitbot::User.new.foo
+  end
 end
