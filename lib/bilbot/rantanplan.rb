f7658b160b9bb8cfbf1cbbf3189992eeb507e694
Martin Delille <martin@phonations.com>
2014-04-13 00:11:58 +0200

    rantanplan log stat to db

--- lib/bilbot/rantanplan.rb
+++ lib/bilbot/rantanplan.rb
@@ -3,6 +3,8 @@ module Bilbot
     def initialize(u, l)
       @user = u
       @list = l
+      db = Bilbot::mongo
+      @coll =db['bbStat'];
     end
 
     def action
@@ -10,6 +12,12 @@ module Bilbot
         u = @list.pop
         @user.follow(u)
       end
+      obj = {
+        "user" => @user.screen_name,
+        "followerCount" => @user.followers_count,
+        "followingCount" => @user.friends_count,
+      }
+      @coll.insert(obj)
      end
 
   end
