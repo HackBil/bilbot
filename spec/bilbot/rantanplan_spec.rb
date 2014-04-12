b3ed4602906855e35206059f12e6127464a06fd7
Alexis Montagne <alexis.montagne@gmail.com>
2014-04-12 23:59:11 +0200

    Refactor the rantanplan test

--- spec/bilbot/rantanplan_spec.rb
+++ spec/bilbot/rantanplan_spec.rb
@@ -1,38 +1,26 @@
 require 'spec_helper'
 
 describe Bilbot do
-  it "should take the first item in the list and follow it", :vcr do
-    user = Bilbot::User.new
-    list = Array.new
-    tobware = 'tobware'
-    tarte_au_sucre = 'tarte_au_sucre'
-    list.push(tobware)
-    list.push(tarte_au_sucre)
-    followingList = user.followings
-    list.each do |id|
-      if followingList.include?(id)
-        user.unfollow(id)
+  let(:user) { Bilbot::User.new }
+  let(:rantanplan) { Bilbot::Rantanplan.new(user, followings) }
+
+  describe '#action' do
+    context 'the queue is empty' do
+      let(:followings) { [] }
+
+      it 'shouldn\'t hit the follow method', :vcr do
+        Bilbot::User.any_instance.should_not_receive(:follow)
+        rantanplan.action
       end
     end
-    rantanplan = Bilbot::Rantanplan.new(user, list)
 
-    list.length.should == 2
-    followingList = user.followings
-    followingList.include?(tobware).should == false
-    followingList.include?(tarte_au_sucre).should == false
+    context 'the queue is not empty' do
+      let(:followings) { ['tobware', 'tarte_au_sucre'] }
 
-    rantanplan.action
-    list.length.should == 1
-
-    followingList = user.followings
-    followingList.include?(tobware).should == false
-    followingList.include?(tarte_au_sucre).should == true
-
-    rantanplan.action
-
-    list.length.should == 0
-    followingList = user.followings
-    followingList.include?(tobware).should == true
-    followingList.include?(tarte_au_sucre).should == true
+      it 'should hit the follow method', :vcr do
+        Bilbot::User.any_instance.should_receive(:follow).with('tarte_au_sucre')
+        rantanplan.action
+      end
+    end
   end
 end
