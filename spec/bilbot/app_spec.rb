962d35fdb78f39ee9f88a1ebff1fd69fb9afd72c
Martin Delille <martin@phonations.com>
2014-04-13 22:42:47 +0200

    test for api /create

--- spec/bilbot/app_spec.rb
+++ spec/bilbot/app_spec.rb
@@ -5,4 +5,13 @@ describe Bilbot::App do
     get '/'
     last_response.should be_ok
   end
+
+  it 'should create a new customer' do
+    Bilbot::Customer.should_receive(:new).with('token', 'secret', 'list').once
+    post '/create', {
+      oauth_token: 'token',
+      oauth_token_secret: 'secret',
+      target_list: 'list',
+    }
+  end
 end
