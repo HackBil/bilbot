d8d1852048b241da51847a30ac14caaca061ff8c
Alexis Montagne <alexis.montagne@gmail.com>
2014-04-13 21:33:45 +0200

    Update customer test suite

--- lib/bilbot/customer.rb
+++ lib/bilbot/customer.rb
@@ -1,46 +1,45 @@
 module Bilbot
   class Customer
-
     attr_accessor :a_token
     attr_accessor :a_secret
 
     attr_accessor :target_list
 
-    attr_accessor :coll
-    COLL = Bilbot.mongo['customers']
-
     def initialize(_a_token, _a_secret, _target_list = [])
-    	@a_secret = _a_secret
-    	@a_token = _a_token
-    	@target_list = _target_list
+      @a_secret = _a_secret
+      @a_token = _a_token
+      @target_list = _target_list
+    end
+
+    def self.coll
+      @coll ||= Bilbot.mongo['customers']
     end
 
     def save
-    	 COLL.update(
-            { a_secret: @a_secret, a_token: @a_token},
-            { a_secret: @a_secret, a_token: @a_token, target_list: @target_list },
-            { :upsert => true }
-        )
+      self.class.coll.update(
+        { a_secret: @a_secret, a_token: @a_token},
+        { a_secret: @a_secret, a_token: @a_token, target_list: @target_list },
+        { :upsert => true }
+      )
     end
 
     # To be Used by API
     def self.create (_a_token, _a_secret, _target_list = [])
-
-    	out = self.new(_a_token, _a_secret, _target_list = [])
-    	out.save
+      out = self.new(_a_token, _a_secret, _target_list = [])
+      out.save
     end
 
     def self.findall
-    	COLL.find.map{ |customer| Customer.new(customer['a_token'], customer['a_secret'], customer['target_list'])}
+      coll.find.map{ |customer| Customer.new(customer['a_token'], customer['a_secret'], customer['target_list'])}
     end
 
     def rest_client
-    	@rest_client ||= Twitter::REST::Client.new do |config|
-      		config.consumer_key        = ENV["CONSUMER_KEY"]
-      		config.consumer_secret     = ENV["CONSUMER_SECRET"]
-		    config.access_token        = @a_token
-      		config.access_token_secret = @a_secret
-    	end
+      @rest_client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["CONSUMER_KEY"]
+        config.consumer_secret     = ENV["CONSUMER_SECRET"]
+        config.access_token        = @a_token
+        config.access_token_secret = @a_secret
+      end
     end
-   end
+  end
 end
--- spec/bilbot/customer_spec.rb
+++ spec/bilbot/customer_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe Bilbot::Customer do
   let(:mongodb) { Bilbot.mongo.collection('customers') }
 
-  	it "should add Customer to DB after instantiation" do
-  		Bilbot::Customer.new('test','test2','test3')
-  		mongodb.find( {  a_secret: 'test2', a_token: 'test'} ).count.should == 1
-  	end
-
+  it "should add Customer to DB after instantiation" do
+    Bilbot::Customer.create('test','test2')
+    mongodb.find( {  a_secret: 'test2', a_token: 'test'} ).count.should == 1
+  end
 end
